9e56795371daf9d141070906d8cb483443e1b683
Maciek Makowski <maciek.makowski@gmail.com>
2010-09-23 19:31:22 +0100

    fixed the lexer a bit

--- src/main/scala/Lexer.scala
+++ src/main/scala/Lexer.scala
@@ -16,7 +16,8 @@ object MarkupSyntax {
 
 trait MarkupTokens extends Tokens {
   case object Backslash extends Token { def chars = "\\" }
-  case object NewLine extends Token { def chars = "\n\n" }
+  case object DoubleNewLine extends Token { def chars = "\\" }
+  case class Asterisks(length: Int) extends Token { def chars = "*" * length }
   case object OpeningBracket extends Token { def chars = "{" }
   case object ClosingBracket extends Token { def chars = "}" }
   // TODO: Whitespace
@@ -30,23 +31,30 @@ class Lexer extends Lexical with MarkupTokens {
 
   def token : Parser[Token] = (
     '\\' ^^^ Backslash
-    | '\r'~'\n' ^^^ NewLine
-    | '\n'~'\r' ^^^ NewLine
-    | '\n' ^^^ NewLine
+    | asterisks
+    | '{' ^^^ OpeningBracket
+    | '}' ^^^ ClosingBracket
+    | doubleNewLine ^^^ DoubleNewLine
     | EofCh ^^^ EOF
     | rep1(chrExcept(EofCh, '\\', '\n', '\r')) ^^ (charList => Str(charList mkString ""))
+    | newLine ^^^ Str("\n") // TODO: ideally this would be part of the preceding Str
   )
+
+  def asterisks : Parser[Token] = rep1('*')<~' ' ^^ (astList => Asterisks(astList.length))
+  def doubleNewLine = newLine~whitespaceIgnorableBeforeNewLine~newLine
+  def newLine = '\r'~'\n' | '\n'~'\r' | '\n' | '\r'
+  def whitespaceIgnorableBeforeNewLine = rep(elem(' ') | '\t')
 }
 
 class Parser extends TokenParsers {
   type Tokens = MarkupTokens
   val lexical = new Lexer
-  import lexical.{Backslash, NewLine, Str}
+  import lexical.{Backslash, Str}
   import MarkupSyntax._
 
   def document : Parser[Document] = rep(paragraph) ^^ 
 				    (elts => Document(elts))
-  def paragraph : Parser[DocumentElement] = rep1(paragraphElement) <~ NewLine ^^ 
+  def paragraph : Parser[DocumentElement] = rep1(paragraphElement) ^^ 
 				   	    (elts => Paragraph(elts))
   def paragraphElement : Parser[ParagraphElement] = text
   def text : Parser[Text] = Str("dupa") ^^ (str => Text(str.asInstanceOf[Str].s))
--- src/test/scala/Lexer.scala
+++ src/test/scala/Lexer.scala
@@ -7,32 +7,49 @@ import org.scalatest.matchers.ShouldMatchers
 
 class LexerSuite extends FunSuite with ShouldMatchers {
   val lexer = new Lexer()
-  def tokenFrom(s: String) = lexer.token(new CharSequenceReader(s)).get
+  def firstTokenFrom(s: String) = lexer.token(new CharSequenceReader(s)).get
+
+  test("asterisks followed by space are recognised as Asterisks token") {
+    firstTokenFrom ("*** some asterisks") should equal (lexer.Asterisks(3))
+  }
+
+  test("asterisks with no space are recognised as Str token") {
+    firstTokenFrom ("***some asterisks") should equal (lexer.Str("***some asterisks"))
+  }
 
   test("backslash token is recognised") {
-    tokenFrom ("\\") should equal (lexer.Backslash)
+    firstTokenFrom ("\\") should equal (lexer.Backslash)
   }
 
   test("lf is recognised as a new line token") {
-    tokenFrom ("\nasdf") should equal (lexer.NewLine)
+    firstTokenFrom ("\n\nasdf") should equal (lexer.DoubleNewLine)
   }
 
   test("cr-lf is recognised as a new line token") {
-    tokenFrom ("\r\nasdf") should equal (lexer.NewLine)
+    firstTokenFrom ("\r\n\r\nasdf") should equal (lexer.DoubleNewLine)
   }
 
-  test("string is scanned until new line") {
-    tokenFrom ("some sample text\nwith a new line") should equal (lexer.Str("some sample text"))
+  test("double new line with whitespace in between is recognised as double new line token") {
+    firstTokenFrom ("\n \t \nsome text") should equal (lexer.DoubleNewLine)
   }
 
-  val textToScan = """* Header
+  test("string is scanned until double new line") {
+    firstTokenFrom ("some sample text\n\nwith a double new line") should equal (lexer.Str("some sample text"))
+  }
+/*
+  test("single new line is included in string") {
+    firstTokenFrom ("some sample text\nwith a single new line") should equal (lexer.Str("some sample text\nwith a single new line"))
+  }
+*/
+  val textToScan = """*** Header
 
 paragraph 1,
       which consists of multiple
 lines.
+  
+paragraph 2 with a \tag{ multi-paragraph
 
-paragraph 2 with \ a backslash
-  """
+tag}"""
 
   test("scan some string") {
     //println(textToScan.replace("\n", "<lf>").replace("\r", "<cr>"))
